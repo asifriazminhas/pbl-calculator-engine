bd0413a48f69680e9f48a6dcc9fa49855041341f
Yulric Sequeira Work Computer <yulrics@gmail.com>
2016-10-05 18:43:58 -0400

    Added op_type model

---
+++ src/models/op_type.ts
@@ -0,0 +1,20 @@
+enum OpType {
+    Continuous=0,
+    Categorical=1
+}
+
+export function getOpTypeFromPmmlOpType(opType: string): OpType {
+    switch(opType) {
+        case 'continuous': {
+            return OpType.Continuous
+        }
+        case 'categorical': {
+            return OpType.Categorical
+        }
+        default: {
+            throw new Error('Unknown Pmml OpType')
+        }
+    }
+}
+
+export default OpType
