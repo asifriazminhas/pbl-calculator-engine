326f0c6e0ff7e0aba07cec6652a59c83987ab8f7
Yulric Sequeira Work Computer <yulrics@gmail.com>
2016-10-06 15:08:32 -0400

    Added method to evaluate algorithm

--- src/models/algorithm.ts
+++ src/models/algorithm.ts
@@ -4,19 +4,70 @@ import ExplanatoryPredictor from './predictors/explanatory_predictor'
 import getPmmlParser, {
     PmmlAlgorithmParser
 } from './parsers/pmml'
+import Datum from './data/datum'
 
 class Algorithm implements PmmlAlgorithmParser {
     explanatoryPredictors: Array<ExplanatoryPredictor>
-    intermediatePredictor: Array<IntermediatePredictor>
+    intermediatePredictors: Array<IntermediatePredictor>
 
     constructFromPmml(explanatoryPredictors: Array<ExplanatoryPredictor>, intermediatePredictors: Array<IntermediatePredictor>): Algorithm {
         this.explanatoryPredictors = explanatoryPredictors
-        this.intermediatePredictor = intermediatePredictors
+        this.intermediatePredictors = intermediatePredictors
 
         return this
     }
 
     static parsePmml: (pmml: string) => Promise<Algorithm> = getPmmlParser(Algorithm, ExplanatoryPredictor, IntermediatePredictor)
+
+    private getExplanatoryPredictorDataForIntermediatePredictor(intermediatePredictor: IntermediatePredictor, data: Array<Datum>): Array<Datum> {
+        return intermediatePredictor.explanatoryPredictors.map((explanatoryPredictor) => {
+            var dataForExplanatoryPredictor = data.find((datum) => {
+                return datum.name ===  explanatoryPredictor
+            })
+
+            if(!dataForExplanatoryPredictor) {
+                var intermediatePredictorForExplanatoryPredictor = this.intermediatePredictors
+                .find((intermediatePredictor) => {
+                    return intermediatePredictor.name === explanatoryPredictor
+                })
+
+                if(!intermediatePredictorForExplanatoryPredictor) {
+                    throw new Error(`No error for predictor ${explanatoryPredictor}`)
+                }
+                else {
+                    return new Datum().constructorForNewDatum(intermediatePredictorForExplanatoryPredictor.name, intermediatePredictorForExplanatoryPredictor.evaluate(this.getExplanatoryPredictorDataForIntermediatePredictor(intermediatePredictorForExplanatoryPredictor, data)))
+                }
+            }
+            else {
+                return dataForExplanatoryPredictor
+            }
+        })
+    }
+
+    evaluate(data: Array<Datum>): number {
+        return this.explanatoryPredictors.reduce((currentValue, explanatoryPredictor) => {
+            let foundDatumForCurrentPredictor = data.find((datum) => {
+                return datum.name === explanatoryPredictor.name
+            })
+
+            if(!foundDatumForCurrentPredictor) {
+                let foundIntermediatePredictor = this.intermediatePredictors
+                .find((intermediatePredictor) => {
+                    return intermediatePredictor.name === explanatoryPredictor.name
+                })
+
+                if(!foundIntermediatePredictor) {
+                    throw new Error(`No predictor found for ${explanatoryPredictor.name}`)
+                }
+                else {
+                    return foundIntermediatePredictor.evaluate(this.getExplanatoryPredictorDataForIntermediatePredictor(foundIntermediatePredictor, data))
+                }
+            }
+            else {
+                return currentValue + foundDatumForCurrentPredictor.coefficent*explanatoryPredictor.beta
+            }
+        }, 0)
+    }
 }
 
 export default Algorithm
