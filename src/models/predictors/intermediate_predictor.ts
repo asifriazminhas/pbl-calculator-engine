225a1850d403be9264133e0f2f0b0f1a5302874a
Yulric Sequeira Work Computer <yulrics@gmail.com>
2016-10-06 15:08:14 -0400

    Renamed intermediate predictor to intermediate predictors and added method to evaluet the equation field

--- src/models/predictors/intermediate_predictor.ts
+++ src/models/predictors/intermediate_predictor.ts
@@ -3,6 +3,8 @@ import {
     PmmlIntermediatePredictorParser
 } from '../parsers/pmml'
 import Predictor from './predictor'
+import HelperFunctions from './helper_functions'
+import Datum from '../data/datum'
 
 class IntermediatePredictor extends Predictor implements PmmlIntermediatePredictorParser {
     equation: string
@@ -15,6 +17,30 @@ class IntermediatePredictor extends Predictor implements PmmlIntermediatePredict
         this.explanatoryPredictors = explanatoryPredictors
 
         return this
+    }
+
+    evaluate(explanatoryPredictorsData: Array<Datum>): number {
+        var obj: {
+            [index: string]: number
+        } = {}
+
+        this.explanatoryPredictors.forEach((explanatoryPredictor) => {
+            var dataForCurrentExplanatoryPredictor = explanatoryPredictorsData
+            .find((explanatoryPredictorData) => {
+                return explanatoryPredictor === explanatoryPredictorData.name
+            })
+
+            if(!dataForCurrentExplanatoryPredictor) {
+                throw new Error(`No data found for predictor ${explanatoryPredictor}`)
+            }
+            else {
+                obj[explanatoryPredictor] = dataForCurrentExplanatoryPredictor.coefficent
+            }
+        })
+
+        var func = HelperFunctions
+
+        return eval(this.equation)
     }
 }
 
