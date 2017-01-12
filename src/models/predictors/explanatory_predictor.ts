1b3adb8e9512177ad8a45f98f8f9013f207dcf60
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-01-12 10:56:47 -0500

    Explanatory Predictor now has customFunction variable

--- src/models/predictors/explanatory_predictor.ts
+++ src/models/predictors/explanatory_predictor.ts
@@ -2,6 +2,7 @@
 import Predictor, {
     PredictorObj
 } from './predictor'
+import CustomFunction from '../custom_functions/custom_function';
 
 export interface ExplanatoryPredictorObj extends PredictorObj {
     beta: number
@@ -11,12 +12,14 @@ export interface ExplanatoryPredictorObj extends PredictorObj {
 class ExplanatoryPredictor extends Predictor {
     beta: number
     referencePoint: number
+    customFunction: CustomFunction<any> | null;
 
-    constructFromPmml(name: string, opType: string, beta: string, referencePoint: string): ExplanatoryPredictor {
+    constructFromPmml(name: string, opType: string, beta: string, referencePoint: string, customFunction: CustomFunction<any> | null): ExplanatoryPredictor {
         super.constructFromNameAndOpType(name, opType)
 
         this.beta = Number(beta)
         this.referencePoint = Number(referencePoint)
+        this.customFunction = customFunction;
 
         return this
     }
