1e23594d6b29ea7174d5b28d6899d7cded36d977
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-01-27 13:59:28 -0500

    Updated pmml parser since DeriedField can have COnstant as root node

--- src/models/parsers/pmml.ts
+++ src/models/parsers/pmml.ts
@@ -1,3 +1,5 @@
+import { getASTForConstant } from './node_parser';
+
 //bluebird
 import * as bluebird from 'bluebird'
 
@@ -31,6 +33,17 @@ import {
 //Returns the js code strings which needs to be evaluated to compute this derived field
 //The DerivedField pmml xml node to get the equation from
 function getDerivedFieldEquation(derivedField: DerivedField): string {
+    let right: any = null;
+    if (derivedField.Apply) {
+        right = getASTForApply(derivedField.Apply);
+    }
+    else if (derivedField.Constant) {
+        right = getASTForConstant(derivedField.Constant);
+    }
+    else {
+        throw new Error(`Unknown root node in derived field`);
+    }
+
     //make the line of code 'var {derivedFieldName};'
     var declarationAst = {
         type: 'ExpressionStatement',
@@ -41,7 +54,7 @@ function getDerivedFieldEquation(derivedField: DerivedField): string {
                 type: 'Identifier',
                 name: 'derived'
             },
-            right: getASTForApply(derivedField.Apply)
+            right
         }
     };
 
@@ -49,9 +62,7 @@ function getDerivedFieldEquation(derivedField: DerivedField): string {
     return escodegen.generate(declarationAst)
 }
 
-//Used to construct a DerivedPredictor. Returns an array of strings where each string corresponds to the name of the predictor which this derive equation needs to be evaluated
-//tag: The pmml xml node to go through to find all the predictor names. Start with a DerivedField node
-function getDerivedFrom(tag: Apply): Array<string> {
+function getDerivedFromForApplyTag(tag: Apply): Array<string> {
     return tag.$$.reduce((currentDerivedFrom: Array<string>, child: ApplyChildNode) => {
         //If this a FieldRef node then it has the name of a predictor which this derived field depends on
         if (child['#name'] === 'FieldRef') {
@@ -60,21 +71,36 @@ function getDerivedFrom(tag: Apply): Array<string> {
         }
         //This node has more child nodes so recursively call this function
         else if ((child as Apply).$$) {
-            currentDerivedFrom = currentDerivedFrom.concat(getDerivedFrom(child as Apply));
+            currentDerivedFrom = currentDerivedFrom
+            .concat(getDerivedFromForApplyTag(child as Apply));
         }
 
         return currentDerivedFrom;
     }, [])
-    //Remove all duplicate predictor names
-    .reduce((currentDerivedFrom: Array<string>, derivedFrom: string) => {
-        if (currentDerivedFrom.indexOf(derivedFrom) < 0)
-            currentDerivedFrom.push(derivedFrom);
+        //Remove all duplicate predictor names
+        .reduce((currentDerivedFrom: Array<string>, derivedFrom: string) => {
+            if (currentDerivedFrom.indexOf(derivedFrom) < 0)
+                currentDerivedFrom.push(derivedFrom);
 
-        return currentDerivedFrom;
-    }, [])
-    .filter((derivedFrom) => {
-        return derivedFrom !== 'NA'
-    })
+            return currentDerivedFrom;
+        }, [])
+        .filter((derivedFrom) => {
+            return derivedFrom !== 'NA'
+        })
+}
+
+//Used to construct a DerivedPredictor. Returns an array of strings where each string corresponds to the name of the predictor which this derive equation needs to be evaluated
+//tag: The pmml xml node to go through to find all the predictor names. Start with a DerivedField node
+function getDerivedFrom(derivedField: DerivedField): Array<string> {
+    if(derivedField.Constant) {
+        return [];
+    }
+    else if(derivedField.Apply) {
+        return getDerivedFromForApplyTag(derivedField.Apply);
+    }
+    else {
+        throw new Error(`Unknown root tag in derivedField`);
+    }
 }
 
 export default async function (Algorithm: {
@@ -92,46 +118,46 @@ export default async function (Algorithm: {
     });
 
     var explanatoryPredictors = parsedPmml.PMML.GeneralRegressionModel.ParamMatrix.PCell
-    .map((pCell) => {
-        var ppCellForCurrentPCell = parsedPmml.PMML.GeneralRegressionModel.PPMatrix.PPCell
-            .find((ppCell) => {
-                return ppCell.$.parameterName === pCell.$.parameterName
-            })
+        .map((pCell) => {
+            var ppCellForCurrentPCell = parsedPmml.PMML.GeneralRegressionModel.PPMatrix.PPCell
+                .find((ppCell) => {
+                    return ppCell.$.parameterName === pCell.$.parameterName
+                })
 
-        if (ppCellForCurrentPCell === undefined) {
-            throw new Error(`No ppCell found for pCell ${pCell.$.parameterName}`)
-        }
-
-        var dataFieldForCurrentPCell = parsedPmml.PMML.DataDictionary.DataField
-        .find((dataField) => {
             if (ppCellForCurrentPCell === undefined) {
                 throw new Error(`No ppCell found for pCell ${pCell.$.parameterName}`)
             }
-            else {
-                return dataField.$.name === ppCellForCurrentPCell.$.predictorName
+
+            var dataFieldForCurrentPCell = parsedPmml.PMML.DataDictionary.DataField
+                .find((dataField) => {
+                    if (ppCellForCurrentPCell === undefined) {
+                        throw new Error(`No ppCell found for pCell ${pCell.$.parameterName}`)
+                    }
+                    else {
+                        return dataField.$.name === ppCellForCurrentPCell.$.predictorName
+                    }
+                })
+
+            var parameterForCurrentPCell = parsedPmml.PMML.GeneralRegressionModel.ParameterList.Parameter
+                .find((parameterList) => {
+                    if (ppCellForCurrentPCell === undefined) {
+                        throw new Error(`No ppCell found for pCell ${pCell.$.parameterName}`)
+                    }
+                    else {
+                        return parameterList.$.name === ppCellForCurrentPCell.$.parameterName
+                    }
+                })
+
+            if (parameterForCurrentPCell === undefined) {
+                throw new Error(`No ParamaterList found for ppCell ${ppCellForCurrentPCell.$.predictorName}`)
             }
+
+            if (dataFieldForCurrentPCell === undefined) {
+                throw new Error(`No DataField found for ppCell ${ppCellForCurrentPCell.$.predictorName}`)
+            }
+
+            return new ExplanatoryPredictor().constructFromPmml(dataFieldForCurrentPCell.$.name, dataFieldForCurrentPCell.$.optype, pCell.$.beta, parameterForCurrentPCell.$.referencePoint, parseCustomFunction(parameterForCurrentPCell, parsedPmml.PMML.GeneralRegressionModel.RestrictedCubicSpline))
         })
-
-        var parameterForCurrentPCell = parsedPmml.PMML.GeneralRegressionModel.ParameterList.Parameter
-        .find((parameterList) => {
-            if (ppCellForCurrentPCell === undefined) {
-                throw new Error(`No ppCell found for pCell ${pCell.$.parameterName}`)
-            }
-            else {
-                return parameterList.$.name === ppCellForCurrentPCell.$.parameterName
-            }
-        })
-
-        if (parameterForCurrentPCell === undefined) {
-            throw new Error(`No ParamaterList found for ppCell ${ppCellForCurrentPCell.$.predictorName}`)
-        }
-
-        if (dataFieldForCurrentPCell === undefined) {
-            throw new Error(`No DataField found for ppCell ${ppCellForCurrentPCell.$.predictorName}`)
-        }
-
-        return new ExplanatoryPredictor().constructFromPmml(dataFieldForCurrentPCell.$.name, dataFieldForCurrentPCell.$.optype, pCell.$.beta, parameterForCurrentPCell.$.referencePoint, parseCustomFunction(parameterForCurrentPCell, parsedPmml.PMML.GeneralRegressionModel.RestrictedCubicSpline))
-    })
 
     var intermediatePredictors: Array<IntermediatePredictor> = []
     if (parsedPmml.PMML.LocalTransformations) {
@@ -139,7 +165,7 @@ export default async function (Algorithm: {
         intermediatePredictors = parsedPmml.PMML.LocalTransformations.DerivedField
             .map((derivedField) => {
                 //Construct the DerivedPredictor object
-                return new IntermediatePredictor().constructFromPmml(derivedField.$.name, derivedField.$.optype, getDerivedFieldEquation(derivedField), getDerivedFrom(derivedField.Apply));
+                return new IntermediatePredictor().constructFromPmml(derivedField.$.name, derivedField.$.optype, getDerivedFieldEquation(derivedField), getDerivedFrom(derivedField));
             });
     }
 
