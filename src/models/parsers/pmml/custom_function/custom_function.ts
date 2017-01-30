5c8ae127eca2b364226ce0769217d80158bac9ac
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-01-30 18:49:53 -0500

    Added module which parses custom functions from pmml

---
+++ src/models/parsers/pmml/custom_function/custom_function.ts
@@ -0,0 +1,23 @@
+import { isRestrictedCubicSplineCustomFunction, parseRcsSpline } from './rcs_spline';
+import CustomFunction from '../../../custom_functions/custom_function';
+import { RestrictedCubicSpline } from '../../../interfaces/pmml/custom/restricted_cubic_spline';
+import { Parameter } from '../../../interfaces/pmml/pmml';
+
+/**
+ * Returns a CustomFunction object if one exists for a predictor or null
+ * 
+ * @export
+ * @param {Parameter} parameter
+ * @param {RestrictedCubicSpline} restrictedCubicSpline
+ * @returns {(CustomFunction<any> | null)}
+ */
+export function parseCustomFunction(parameter: Parameter, restrictedCubicSpline: RestrictedCubicSpline): CustomFunction<any> | null {
+    //Is it a Spline custom function? If it is
+    if(isRestrictedCubicSplineCustomFunction(parameter.$.label)) {
+        return parseRcsSpline(parameter, restrictedCubicSpline);
+    }
+    //Don't know what custom function it is. So return null.
+    else {
+        return null;
+    }
+}
