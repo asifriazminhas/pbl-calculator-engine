66aef101917ac3e04946c781642b114aee05084e
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-01-30 18:48:43 -0500

    Added module which exports function to parse an rcs custom function from pmmml

---
+++ src/models/parsers/pmml/custom_function/rcs_spline.ts
@@ -0,0 +1,80 @@
+import RCSSpline from '../../../custom_functions/rcs_spline';
+import { RestrictedCubicSpline } from '../../../interfaces/pmml/custom/restricted_cubic_spline';
+import { Parameter } from '../../../interfaces/pmml/pmml';
+
+/**
+ * Given a the label field for a Parameter XML node it checks if this predictor has an RCS custom function or not. Eg. age_rcs2 has an rcs function 
+ * 
+ * @export
+ * @param {string} parameterLabel
+ * @returns
+ */
+export function isRestrictedCubicSplineCustomFunction(parameterLabel: string) {
+    return parameterLabel.indexOf('rcs') > -1;
+}
+
+/**
+ * Gets the variable number of this rcs custom function. Eg. age_rcs2 will return 2
+ * 
+ * @param {string} paramaterLabel
+ * @returns
+ */
+function getSplineVariableNumber(paramaterLabel: string) {
+    //Get the variable number for this spline custom function. eg. age_rcs1 it's 1
+    return Number(paramaterLabel.split('rcs')[1]);
+}
+
+/**
+ * Returns the array of knots from the knotLocations field in a RestrictedCubicSpline.PCell node
+ * 
+ * @param {string} knotLocations In the PMML file it is given as '[1, 2, 3, 4]'
+ * @returns {Array<number>}
+ */
+function parseKnotLocations(knotLocations: string): Array<number> {
+    return knotLocations.split(', ').map((knotLocation) => {
+        return Number(knotLocation);
+    });
+}
+
+/**
+ * Returns the predictor name that represents the first rcs variable. Eg. age_rcs2 it would return age_rcs1
+ * 
+ * @param {string} parameterLabel
+ * @returns {string}
+ */
+function parseFirstVariableName(parameterLabel: string): string {
+    return parameterLabel.split('_rcs')[0].concat('_rcs1');
+}
+
+/**
+ * Returns an RCSSpline object parsed from PMMML
+ * 
+ * @export
+ * @param {Parameter} parameter
+ * @param {RestrictedCubicSpline} restrictedCubicSpline
+ * @returns
+ */
+export function parseRcsSpline(parameter: Parameter, restrictedCubicSpline: RestrictedCubicSpline) {
+    const splineVariableNumber = getSplineVariableNumber(parameter.$.label);
+
+    //If it's 1 then we don't have to apply the spline function on it since the component can be calculated normally
+    if (splineVariableNumber === 1) {
+        return null;
+    }
+    //Otherwise
+    else {
+        //Get the RestrictredCubicSpline PCell for this predictor  using the parameter name field
+        const restrictedCubicSplinePCell = restrictedCubicSpline.PCell.find((pCell) => {
+            return pCell.$.parameterName.indexOf(parameter.$.name) > -1;
+        });
+
+        //If there isn't one then we have a problem
+        if (!restrictedCubicSplinePCell) {
+            throw new Error(`No Restricted Cubic Spline Cell found for paramater ${parameter.$.name}`)
+        }
+        //Otherwise Return the Spline object
+        else {
+            return new RCSSpline().constructFromPmml(parseKnotLocations(restrictedCubicSplinePCell.$.knotLocations), parseFirstVariableName(parameter.$.label), splineVariableNumber);
+        }
+    }
+}
