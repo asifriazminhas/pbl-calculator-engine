0a98171225ae93482750e52e20eb2ff6e6bb64e7
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-02-01 14:15:18 -0500

    Added NoDataFoundError

---
+++ src/models/errors/no_data_found_error.ts
@@ -0,0 +1,14 @@
+/**
+ * Error to use when there is not enough data to evaluate the component for an ExplanatoryPredictor or the transformation for an IntermediatePredictor 
+ * 
+ * @export
+ * @class NoDataFoundError
+ * @extends {Error}
+ */
+export class NoDataFoundError extends Error {
+    constructor(predictorName: string) {
+        super(`No data found for predictor ${predictorName}`);
+
+        this.name = 'NoDataFoundError';
+    }
+}
