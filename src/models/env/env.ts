11a2e2094ca3a9d4456be5062de8c69deb5fa4a5
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-02-06 16:53:07 -0500

    Added jdoc comments to Env class. Added more environments (development and debugging) and removed testing environment

--- src/models/env/env.ts
+++ src/models/env/env.ts
@@ -2,24 +2,94 @@ import {
     autobind
 } from 'core-decorators';
 
-export type environment = 'production' | 'testing';
+export type environment = 'production' | 'development' | 'debugging';
 
+/**
+ * Has variables related to the environment in which the engine is currently running
+ * 
+ * @export
+ * @class Env
+ */
 @autobind
-export class Env {
+export class Env { 
+    /**
+     * The environment in which it is currently running
+     * Production: Only errors are logged
+     * Development: Only errors and warnings are logged
+     * Debugging: Error, warnings as well as extra information (like evaluating intermediate predictors, explanatory predictors) are logged
+     * @private
+     * @type {environment}
+     * @memberOf Env
+     */
     private _environment: environment
 
     constructor() {
         this._environment = 'production';
     }
-
-    set environment(environment: environment) {
-        this._environment = environment
+    
+    /**
+     * 
+     * 
+     * 
+     * @memberOf Env
+     */
+    setEnvironmentToProduction() {
+        this._environment = 'production'
     }
-
-    isEnvironmentTesting() {
-        return this._environment === 'testing';
+    
+    /**
+     * 
+     * 
+     * 
+     * @memberOf Env
+     */
+    setEnvironmentToDevelopment() {
+        this._environment = 'development';
+    }
+    
+    /**
+     * 
+     * 
+     * 
+     * @memberOf Env
+     */
+    setEnvironmentToDebugging() {
+        this._environment = 'debugging';
+    }
+    
+    /**
+     * 
+     * 
+     * @returns {boolean}
+     * 
+     * @memberOf Env
+     */
+    isEnvironmentProduction(): boolean {
+        return this._environment === 'production';
+    }
+    
+    /**
+     * 
+     * 
+     * @returns {boolean}
+     * 
+     * @memberOf Env
+     */
+    isEnvironmentDevelopment(): boolean {
+        return this._environment === 'development';
+    }
+    
+    /**
+     * 
+     * 
+     * @returns {boolean}
+     * 
+     * @memberOf Env
+     */
+    isEnvironmentDebugging(): boolean {
+        return this._environment === 'debugging';
     }
 }
 
-export default new Env();
+export const env = new Env();
 
