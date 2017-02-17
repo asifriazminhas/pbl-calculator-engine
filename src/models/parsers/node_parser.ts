34038ca10f436d66aca840ecf2da2d1d2c974624
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-02-17 09:44:36 -0500

    Updated node_parser and added comments

--- src/models/parsers/node_parser.ts
+++ src/models/parsers/node_parser.ts
@@ -5,12 +5,6 @@ import {
     Constant
 } from '../interfaces/pmml/pmml'
 import {
-    BinaryExpressionApply,
-    IfApply,
-    LogicalExpressionApply,
-    CallExpressionApply
-} from '../interfaces/pmml/apply'
-import {
     LiteralAST,
     BinaryExpressionASTLeftAndRight,
     BinaryExpressionAST,
@@ -21,7 +15,7 @@ import {
     ConditionalExpressionAST,
     CallExpressionAST,
     AST
-} from '../interfaces/ast'
+} from '../interfaces/pmml/ast'
 
 //models
 import {
@@ -34,13 +28,24 @@ import {
     getCallExpressionAST
 } from './ast'
 
+/**
+ * Converts a Constant node either into a UnaryExpressionAST (since some of the constants can be negative numbers) or LiteralAST
+ * 
+ * @export
+ * @param {Constant} constant
+ * @returns {(UnaryExpressionAST | LiteralAST)}
+ */
 export function getASTForConstant(constant: Constant): UnaryExpressionAST | LiteralAST {
+    //If the constant's dataType is a string or the dataType is not given
     if(!constant.$ || constant.$.dataType === 'string') {
         return getLiteralAST(constant._)
     }
+    //If it's dataType is a double
     else if(constant.$.dataType === 'double') {
+        //Parse the value as a number
         let value = Number(constant._)
 
+        //If it is negative then return a UnaryExpressionAST
         if(value < 0) {
             return getUnaryExpressionAST('-', getLiteralAST(Math.abs(value)))
         }
@@ -53,36 +58,48 @@ export function getASTForConstant(constant: Constant): UnaryExpressionAST | Lite
     }
 }
 
+/**
+ * Parses a FieldRef node
+ * 
+ * @export
+ * @param {FieldRef} fieldRef
+ * @returns {MemberExpressionAST}
+ */
 export function getASTForFieldRef(fieldRef: FieldRef): MemberExpressionAST {
+    //Since field ref's refer to other predictor values we need to inject them at runtime when evaluating an algorithm. This if the fieldRef is for example test we return the AST so that it generates obj['test']
     return getMemberExpressionAST(getLiteralAST(fieldRef.$.field), 'obj')
 }
 
-const SpecialFunctions: Array<string> = [
-    'exp',
-    'ln',
-    'is.na',
-    'not',
-    'notEqual',
-    'formatDatetime'
-]
-export function getASTForApply(apply: Apply): AST {
+/**
+ * Returns AST for an Apply node
+ * 
+ * @export
+ * @param {Apply} apply
+ * @returns {AST}
+ */
+export function getASTForApply(apply: Apply): BinaryExpressionAST | LogicalExpressionAST | ConditionalExpressionAST | CallExpressionAST {
+    //if the function is a binary expression operator
     if(BinaryExpressionOperators[apply.$.function] !== undefined) {
-        return getASTForBinaryExpressionApply(apply as BinaryExpressionApply)
+        return getASTForBinaryExpressionApply(apply)
     }
+    //if the function is a logical expression operator
     else if(LogicalExpressionOperators[apply.$.function] !==  undefined) {
-        return getASTForLogicalExpressionApply(apply as LogicalExpressionApply)
+        return getASTForLogicalExpressionApply(apply)
     }
+    //if the function is an if statement
     else if(apply.$.function === 'if') {
-        return getASTForIfApply(apply as IfApply)
+        return getASTForIfApply(apply)
     }
+    //if it is one of the special PMML functions
     else if(SpecialFunctions.indexOf(apply.$.function) > -1) {
-        return getASTForCallExpressionApply(apply as CallExpressionApply)
+        return getASTForCallExpressionApply(apply)
     }
     else {
         throw new Error(`Unhandled function ${apply.$.function}`)
     }
 }
 
+//Maps PMML functions which can be binary expression operators to their relevant ones in javascript
 const BinaryExpressionOperators: {
     [index: string]: string | undefined
 } = {
@@ -96,7 +113,14 @@ const BinaryExpressionOperators: {
     'greaterOrEqual': '>=',
     'lessOrEqual': '<='
 }
-export function getASTForBinaryExpressionApply(apply: BinaryExpressionApply): BinaryExpressionAST | UnaryExpressionAST {
+/**
+ * Maps an Apply node whose function is a binary expression operand to a BinaryExpressionAST 
+ * 
+ * @export
+ * @param {Apply} apply
+ * @returns {BinaryExpressionAST}
+ */
+export function getASTForBinaryExpressionApply(apply: Apply): BinaryExpressionAST {
     var left: BinaryExpressionASTLeftAndRight
     var leftNode = apply.$$[0]
     switch(leftNode['#name']) {
@@ -109,7 +133,7 @@ export function getASTForBinaryExpressionApply(apply: BinaryExpressionApply): Bi
             break
         }
         case 'Apply': {
-            left = getASTForApply(leftNode as Apply)
+            left = getASTForApply(leftNode as Apply) as BinaryExpressionASTLeftAndRight
             break
         }
         default: {
@@ -118,12 +142,7 @@ export function getASTForBinaryExpressionApply(apply: BinaryExpressionApply): Bi
     }
 
     var right: BinaryExpressionASTLeftAndRight
-    var rightNode = apply.$$[1]
-
-    if(!rightNode) {
-        return getUnaryExpressionAST(BinaryExpressionOperators[apply.$.function] as string, left as any);
-    }
-
+    var rightNode = apply.$$[1];
     switch(rightNode['#name']) {
         case 'Constant': {
             right = getASTForConstant(rightNode as Constant)
@@ -134,7 +153,7 @@ export function getASTForBinaryExpressionApply(apply: BinaryExpressionApply): Bi
             break
         }
         case 'Apply': {
-            right = getASTForApply(rightNode as Apply)
+            right = getASTForApply(rightNode as Apply) as BinaryExpressionASTLeftAndRight
             break
         }
         default: {
@@ -149,15 +168,38 @@ export function getASTForBinaryExpressionApply(apply: BinaryExpressionApply): Bi
     return getBinaryExpressionAST(BinaryExpressionOperators[apply.$.function] as string, left, right)
 }
 
+//Maps PMML Apply function strings which are logical expression operators to their corresponsing logical expression operand in JS
 const LogicalExpressionOperators: {
     [index: string]: string | undefined
 } = {
     and: '&&',
     or: '||'
 }
-export function getASTForLogicalExpressionApply(apply: LogicalExpressionApply): LogicalExpressionAST {
-    if(apply.$$[0]['#name'] !== 'Apply') {
-        throw new Error(`Unhandled node type ${apply.$$[0]['#name']}`)
+/**
+ * Maps an Apply whose function string is one of the above LogicalExpressionOperators to a LogicalExpressionAST
+ * 
+ * @export
+ * @param {Apply} apply
+ * @returns {LogicalExpressionAST}
+ */
+export function getASTForLogicalExpressionApply(apply: Apply): LogicalExpressionAST {
+    var left: LogicalExpressionASTLeftAndRight
+    switch(apply.$$[1]['#name']) {
+        case 'Constant': {
+            left = getASTForConstant(apply.$$[0] as Constant)
+            break
+        }
+        case 'FieldRef': {
+            left = getASTForFieldRef(apply.$$[0] as FieldRef)
+            break
+        }
+        case 'Apply': {
+            left = getASTForApply(apply.$$[0] as Apply) as LogicalExpressionASTLeftAndRight
+            break
+        }
+        default: {
+            throw new Error(`Unhandled node ${apply.$$[1]['#name']}`)
+        }
     }
 
     var right: LogicalExpressionASTLeftAndRight
@@ -171,7 +213,7 @@ export function getASTForLogicalExpressionApply(apply: LogicalExpressionApply): 
             break
         }
         case 'Apply': {
-            right = getASTForApply(apply.$$[1] as Apply)
+            right = getASTForApply(apply.$$[1] as Apply) as LogicalExpressionASTLeftAndRight
             break
         }
         default: {
@@ -183,10 +225,17 @@ export function getASTForLogicalExpressionApply(apply: LogicalExpressionApply): 
         throw new Error(`Unhandled operator ${apply.$.function}`)
     }
 
-    return getLogicalExpressionAST(LogicalExpressionOperators[apply.$.function] as string, getASTForApply(apply.$$[0] as Apply) as LogicalExpressionASTLeftAndRight, right)
+    return getLogicalExpressionAST(LogicalExpressionOperators[apply.$.function] as string, left, right)
 } 
 
-export function getASTForIfApply(apply: IfApply): ConditionalExpressionAST {
+/**
+ * Maps an Apply node whose function is an if to a ConditionalExpressionAST. Do this rather than an IfStatement because it's more concise
+ * 
+ * @export
+ * @param {Apply} apply
+ * @returns {ConditionalExpressionAST}
+ */
+export function getASTForIfApply(apply: Apply): ConditionalExpressionAST {
     var test: AST
     switch(apply.$$[0]['#name']) {
         case 'Constant': {
@@ -247,8 +296,27 @@ export function getASTForIfApply(apply: IfApply): ConditionalExpressionAST {
     return getConditionalExpressionAST(test, consequent, alternate)
 }
 
-export function getASTForCallExpressionApply(apply: CallExpressionApply): CallExpressionAST {
-    return getCallExpressionAST(getMemberExpressionAST(getLiteralAST(apply.$.function), 'func'), apply.$$.map((apply) => {
+//These are functions which we have implemented ourselves
+const SpecialFunctions: Array<string> = [
+    'exp',
+    'ln',
+    'is.na',
+    'not',
+    'notEqual',
+    'formatDatetime'
+]
+/**
+ * Maps a PMML apply node whose function string is set to one in the above SpecialFunctions object to a CallExpressionAST
+ * 
+ * @export
+ * @param {Apply} apply
+ * @returns {CallExpressionAST}
+ */
+export function getASTForCallExpressionApply(apply: Apply): CallExpressionAST {
+    //We make the function call look like func[apply.$.function] so that we can dynamically make the functions available at runtime
+    return getCallExpressionAST(getMemberExpressionAST(getLiteralAST(apply.$.function), 'func'), 
+    //Go through all the function arguments
+    apply.$$.map((apply) => {
         switch(apply['#name']) {
             case 'Constant': {
                 return getASTForConstant(apply as Constant)
