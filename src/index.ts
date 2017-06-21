f2699dd5febc8ed283c1e45611367455ac640426
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-06-21 17:58:07 -0400

    Fixed src/index module

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Env, env } from './models/env/env';
-import Algorithm from './models/algorithm';
-import { constructFromAlgorithmJson } from './models/parsers/json/algorithm';
+import { Algorithm } from './models/algorithm/algorithm';
+import { parseFromAlgorithmJson } from './models/parsers/json/algorithm';
 
 export {
     env,
     Env,
     Algorithm,
-    constructFromAlgorithmJson
+    parseFromAlgorithmJson
 };
