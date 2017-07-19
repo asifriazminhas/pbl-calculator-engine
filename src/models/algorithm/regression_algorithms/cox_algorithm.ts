372c1fa1d870dcf7591e91aa9b1617d0eb741474
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-07-19 11:40:27 -0400

    Added CoxAlgorithm

---
+++ src/models/algorithm/regression_algorithms/cox_algorithm.ts
@@ -0,0 +1,26 @@
+import { RegressionAlgorithm } from './regression_algorithm';
+import { Datum } from '../../data/datum';
+import { env } from '../../env/env';
+
+export class CoxAlgorithm extends RegressionAlgorithm {
+    evaluate(data: Array<Datum>): number {
+        if (env.shouldLogDebugInfo() === true) {
+            console.groupCollapsed(`Predictors`)
+        }
+
+        if (env.shouldLogDebugInfo()) {
+            console.log(`Baseline Hazard: ${this.baselineHazard}`);
+        }
+
+        if (env.shouldLogDebugInfo() === true) {
+            console.groupEnd();
+        }
+
+        const score = this.calculateScore(data);
+
+        return 1 - Math.pow(
+            Math.E,
+            -1 * this.baselineHazard * Math.pow(Math.E, score)
+        );
+    }
+}
