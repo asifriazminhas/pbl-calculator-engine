2882e251fa64c40c9439c9e47273c25f8bec204b
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-07-19 11:40:19 -0400

    Added RegressionAlgorithm abstract class

---
+++ src/models/algorithm/regression_algorithms/regression_algorithm.ts
@@ -0,0 +1,14 @@
+import { Algorithm } from '../algorithm';
+import { Datum } from '../../data/datum';
+import { env } from '../../env/env';
+import { flatten, add } from 'lodash';
+
+export abstract class RegressionAlgorithm extends Algorithm {
+    calculateScore(data: Array<Datum>): number {
+        return this.covariates
+            .map(covariate => covariate.getComponent(data))
+            .reduce(add)
+    }
+
+    abstract evaluate(data: Array<Datum>): number
+}
