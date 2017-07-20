5381afa257d181a87d3dacb9df9cae131c3ff58a
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-07-20 14:05:03 -0400

    Updated pmml parser to set the type on the AlgorithmJson field

--- src/models/parsers/pmml/pmml.ts
+++ src/models/parsers/pmml/pmml.ts
@@ -3,9 +3,25 @@ import { parseDerivedFields } from './data_fields/derived_field/derived_field';
 import { Pmml, PmmlParser } from '../../pmml';
 import { AlgorithmJson } from '../json/algorithm';
 import { parseFromAlgorithmJson } from '../json/algorithm';
+import { AlgorithmType } from '../json/algorithm_type';
+import { GeneralRegressionModelType, CoxRegressionModelType, LogisticRegressionModelType } from '../../pmml/general_regression_model/general_regression_model';
 
 function parseBaselineHazardFromPmmlXml(pmml: Pmml): number {
     return Number(pmml.pmmlXml.PMML.GeneralRegressionModel.$.baselineHazard);
+}
+
+function getAlgorithmTypeFromModelType(modelType: GeneralRegressionModelType): AlgorithmType {
+    if (modelType === CoxRegressionModelType) {
+        return AlgorithmType.CoxProportionalHazard
+    }
+    else if (modelType === LogisticRegressionModelType) {
+        return AlgorithmType.LogisticRegression;
+    }
+    else {
+        throw new Error(
+            `Unknown modelType ${modelType} for GeneralRegressionModel`
+        );
+    }
 }
 
 export async function pmmlXmlStringsToJson(pmmlXmlStrings: Array<string>): Promise<AlgorithmJson> {
@@ -13,6 +29,9 @@ export async function pmmlXmlStringsToJson(pmmlXmlStrings: Array<string>): Promi
         .parsePmmlFromPmmlXmlStrings(pmmlXmlStrings)
 
     const parsedAlgorithm = {
+        type: getAlgorithmTypeFromModelType(
+            pmml.pmmlXml.PMML.GeneralRegressionModel.$.modelType
+        ),
         name: pmml.pmmlXml.PMML.Header.Extension.ModelName,
         version: pmml.pmmlXml.PMML.Header.Extension.Version,
         description: pmml.pmmlXml.PMML.Header.$.description,
