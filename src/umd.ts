11b0cf64c1a502b2071806bea27a6e1fd295fbf0
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-07-25 14:52:59 -0400

    Added index file for the umb build

---
+++ src/umd.ts
@@ -0,0 +1,4 @@
+export { Env, env } from './models/env/env';
+export { Algorithm } from './models/algorithm/algorithm';
+export { parseFromAlgorithmJson, AlgorithmJson } from './models/parsers/json/algorithm';
+export { constructLifeExpectancyFunctionForAlgorithm } from './models/modules/life_table';
