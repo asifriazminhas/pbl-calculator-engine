59e9cc09faed2187b472d83ff3db6a938f65c3d8
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-09-26 13:45:04 -0400

    Added test for life table calculations

---
+++ src/test/life-table.ts
@@ -0,0 +1,51 @@
+var csvParse = require('csv-parse/lib/sync');
+import { getCompleteLifeTableWithStartAge } from '../engine/common/life-table';
+import * as path from 'path';
+import * as fs from 'fs';
+import { expect } from 'chai';
+
+describe(`Life table methods`, function() {
+    before(function() {
+        this.testLifeTable = csvParse(
+            fs.readFileSync(path.join(
+                __dirname, '../../assets/test/life_table.csv'
+            ), 'utf8'), 
+            {
+                columns: true
+            }
+        );
+        this.calculatedLifeTable = getCompleteLifeTableWithStartAge(
+            this.testLifeTable,
+            (age) => {
+                const ageRowFromTestLifeTable = this.testLifeTable
+                    .find((testLifeTableRow: any) => {
+                        return testLifeTableRow.age === age
+                    });
+                if(!ageRowFromTestLifeTable) {
+                    throw new Error();
+                }
+
+                return 1 - Number(ageRowFromTestLifeTable.qx);
+            },
+            this.testLifeTable[0].age
+        );
+    });
+
+    it(`should properly calculate the life table fields`, function() {
+        this.testLifeTable.forEach((testLifeTableRow: any, index: number) => {
+            const fields = [
+                'ex'
+            ];
+
+            fields.forEach((field) => {
+                const diff = this.calculatedLifeTable[index][field] - Number(
+                    testLifeTableRow[field]
+                );
+                const errorPercentage = diff/Number(testLifeTableRow[field]);
+                const allowedErrorPercentage = 0.01;
+
+                expect(errorPercentage).to.be.lessThan(allowedErrorPercentage)
+            });
+        });
+    });
+});
