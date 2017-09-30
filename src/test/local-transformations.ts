c832ffe31cf7084863d6c04f2c8508fa995b1998
yulricsequeira <yulrics@gmail.com>
2017-09-30 18:51:44 -0400

    Updated local-transformations test file to run through all csv files at once

--- src/test/local-transformations.ts
+++ src/test/local-transformations.ts
@@ -8,9 +8,8 @@ const assetsDir = path.join(__dirname, '../../assets/test');
 import { expect } from 'chai';
 import * as fs from 'fs';
 import { Data } from '../engine/common/data';
-import { } from '../engine/cox/covariate';
 
-describe.only(`Testing local transformations`, function() {
+describe(`Testing local transformations`, function() {
     function formatInputData(inputData: {
         [index: string]: any
     }): Data {
@@ -46,105 +45,118 @@ describe.only(`Testing local transformations`, function() {
             ))
             .toJson();
         const cox = parseCoxJsonToCox(coxJson);
-        
-        const localTransformationsTestingData: Array<{
-            [index: string]: string
-        }> = csvParse(
-            fs.readFileSync(
-                path.join(`${assetsDir}/local-transformations/smoketest.csv`),
-                'utf8'
-            ),
-            {
-                columns: true
-            }
-        );
 
-        const nonCovariateColumnsInTestingData = Object
-            .keys(localTransformationsTestingData[0])
-            .filter((localTransformationsTestingDataColumnName) => {
-                const covariateWithSameColumnName = cox
-                    .covariates
-                    .find(covariate => covariate.name === localTransformationsTestingDataColumnName);
-
-                return covariateWithSameColumnName ? false : true;
-            });
-        const covariateColumnsInTestingData = Object
-            .keys(localTransformationsTestingData[0])
-            .filter((localTransformationsTestingDataColumnName) => {
-                return nonCovariateColumnsInTestingData
-                    .indexOf(localTransformationsTestingDataColumnName) === -1 && localTransformationsTestingDataColumnName != "";
-            });
-
-        const testingDataWithoutCovariateColumns: Array<any> = [];
-        localTransformationsTestingData
-            .forEach((localTransformationsTestingDatum) => {
-                const testingDatum: any = {};
-                nonCovariateColumnsInTestingData.forEach((columnName) => {
-                    testingDatum[columnName] = localTransformationsTestingDatum[columnName]
-                });
-
-                testingDataWithoutCovariateColumns.push(testingDatum)
-            });
-
-        const testingDataWithCovariateColumns: Array<any> = [];
-        localTransformationsTestingData
-            .forEach((localTransformationsTestingDatum) => {
-                const testingDatum: any = {};
-                covariateColumnsInTestingData.forEach((columnName) => {
-                    testingDatum[columnName] = localTransformationsTestingDatum[columnName]
-                });
-
-                testingDataWithCovariateColumns.push(testingDatum)
-            });
-
-        this.input = testingDataWithoutCovariateColumns;
-        this.output = testingDataWithCovariateColumns;
         this.cox = cox;
     });
 
-    it(`should properly the local transformations`, function() {
-        const cox: Cox = this.cox;
-        const outputKeys: Array<string> = Object.keys(this.output[0]);
-        const derivedFieldForEachOutputKey = outputKeys
-            .map((outputKey) => {
-                const covariateFound = cox
-                    .covariates
-                    .find(covariate => covariate.name === outputKey);
-                if(!covariateFound) {
-                    throw new Error('')
-                }
+    function runLocalTransformationsTests() {
+        const localTransformationTestingFiles = path
+            .join(`${assetsDir}/local-transformations`)
+        fs.readdirSync(localTransformationTestingFiles)
+        .filter(fileName => fileName !== '.DS_Store')
+        .forEach((csvTestingFile) => {
+            before(async function() {
+                const cox: Cox = this.cox;
 
-                if(!covariateFound.derivedField) {
-                    throw new Error('')
-                }
-
-                return covariateFound.derivedField;
+                const localTransformationsTestingData: Array<{
+                    [index: string]: string
+                }> = csvParse(
+                    fs.readFileSync(`${localTransformationTestingFiles}/${csvTestingFile}`),
+                    {
+                        columns: true
+                    }
+                );
+        
+                const nonCovariateColumnsInTestingData = Object
+                    .keys(localTransformationsTestingData[0])
+                    .filter((localTransformationsTestingDataColumnName) => {
+                        const covariateWithSameColumnName = cox
+                            .covariates
+                            .find(covariate => covariate.name === localTransformationsTestingDataColumnName);
+        
+                        return covariateWithSameColumnName ? false : true;
+                    });
+                const covariateColumnsInTestingData = Object
+                    .keys(localTransformationsTestingData[0])
+                    .filter((localTransformationsTestingDataColumnName) => {
+                        return nonCovariateColumnsInTestingData
+                            .indexOf(localTransformationsTestingDataColumnName) === -1 && localTransformationsTestingDataColumnName != "";
+                    });
+        
+                const testingDataWithoutCovariateColumns: Array<any> = [];
+                localTransformationsTestingData
+                    .forEach((localTransformationsTestingDatum) => {
+                        const testingDatum: any = {};
+                        nonCovariateColumnsInTestingData.forEach((columnName) => {
+                            testingDatum[columnName] = localTransformationsTestingDatum[columnName]
+                        });
+        
+                        testingDataWithoutCovariateColumns.push(testingDatum)
+                    });
+        
+                const testingDataWithCovariateColumns: Array<any> = [];
+                localTransformationsTestingData
+                    .forEach((localTransformationsTestingDatum) => {
+                        const testingDatum: any = {};
+                        covariateColumnsInTestingData.forEach((columnName) => {
+                            testingDatum[columnName] = localTransformationsTestingDatum[columnName]
+                        });
+        
+                        testingDataWithCovariateColumns.push(testingDatum)
+                    });
+        
+                this.input = testingDataWithoutCovariateColumns;
+                this.output = testingDataWithCovariateColumns;
+                this.cox = cox;
             });
-
-        this.input.forEach((inputData: any, index: number) => {
-            const outputForCurrentInput = this.output[index];
-            const data: Data = formatInputData(inputData)
-
-            derivedFieldForEachOutputKey
-                .forEach((derivedField) => {
-                    const actualOutput = calculateCoefficent(
-                        derivedField, data, cox.userFunctions
-                    );
-                    let expectedOutput = outputForCurrentInput[derivedField.name];
-                    if(expectedOutput === 'NA') {
-                        expectedOutput = null
-                    }
-
-                    let diffError = (expectedOutput - (actualOutput as number))/expectedOutput;
-                    if(expectedOutput == 0 && actualOutput == 0) {
-                        diffError = 0;
-                    }
-                    else if(expectedOutput === null && actualOutput === null) {
-                        diffError = 0;
-                    }
-
-                    expect(diffError < 0.00001 || diffError === 0, `DerivedField ${derivedField.name}. Input Row: ${index}. Actual Output: ${actualOutput}. ExpectedOutput: ${expectedOutput}. DiffError: ${diffError}`).to.be.true
+        
+            it(`should properly calculate the local transformations for file ${csvTestingFile}`, function() {
+                const cox: Cox = this.cox;
+                const outputKeys: Array<string> = Object.keys(this.output[0]);
+                const derivedFieldForEachOutputKey = outputKeys
+                    .map((outputKey) => {
+                        const covariateFound = cox
+                            .covariates
+                            .find(covariate => covariate.name === outputKey);
+                        if(!covariateFound) {
+                            throw new Error('')
+                        }
+        
+                        if(!covariateFound.derivedField) {
+                            throw new Error('')
+                        }
+        
+                        return covariateFound.derivedField;
+                    });
+        
+                this.input.forEach((inputData: any, index: number) => {
+                    const outputForCurrentInput = this.output[index];
+                    const data: Data = formatInputData(inputData)
+        
+                    derivedFieldForEachOutputKey
+                        .forEach((derivedField) => {
+                            const actualOutput = calculateCoefficent(
+                                derivedField, data, cox.userFunctions
+                            );
+                            let expectedOutput = outputForCurrentInput[derivedField.name];
+                            if(expectedOutput === 'NA') {
+                                expectedOutput = null
+                            }
+        
+                            let diffError = (expectedOutput - (actualOutput as number))/expectedOutput;
+                            if(expectedOutput == 0 && actualOutput == 0) {
+                                diffError = 0;
+                            }
+                            else if(expectedOutput === null && actualOutput === null) {
+                                diffError = 0;
+                            }
+        
+                            expect(diffError < 0.00001 || diffError === 0, `DerivedField ${derivedField.name}. Input Row: ${index}. Actual Output: ${actualOutput}. ExpectedOutput: ${expectedOutput}. DiffError: ${diffError}`).to.be.true
+                        });
                 });
+            });
         });
-    });
+    }
+
+    runLocalTransformationsTests();
 });
