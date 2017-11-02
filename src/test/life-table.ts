88e6400d503cd5b6f34601d40fc86bad314954fe
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-11-02 16:28:41 -0400

    [Refactor] Tapefied life-table tests

--- src/test/life-table.ts
+++ src/test/life-table.ts
@@ -1,57 +1,64 @@
-var csvParse = require('csv-parse/lib/sync');
-import { getCompleteLifeTableWithStartAge } from '../engine/life-table';
+// tslint:disable-next-line
+const csvParse = require('csv-parse/lib/sync');
+import {
+    getCompleteLifeTableWithStartAge,
+    CompleteLifeTableRow,
+} from '../engine/life-table';
 import * as path from 'path';
 import * as fs from 'fs';
 import { expect } from 'chai';
+import * as test from 'tape';
 
-describe(`Life table methods`, function() {
-    before(function() {
-        this.testLifeTable = csvParse(
-            fs.readFileSync(path.join(
-                __dirname, '../../assets/test/life-table/life_table.csv'
-            ), 'utf8'), 
-            {
-                columns: true
+test(`LifeTable.getCompleteLifeTableWithStartAge`, t => {
+    const testLifeTable = csvParse(
+        fs.readFileSync(
+            path.join(__dirname, '../../assets/test/life-table/life_table.csv'),
+            'utf8',
+        ),
+        {
+            columns: true,
+        },
+    );
+
+    const calculatedLifeTable = getCompleteLifeTableWithStartAge(
+        testLifeTable,
+        age => {
+            const ageRowFromTestLifeTable = testLifeTable.find(
+                (testLifeTableRow: any) => {
+                    return testLifeTableRow.age === age;
+                },
+            );
+            if (!ageRowFromTestLifeTable) {
+                throw new Error();
             }
-        );
-        this.calculatedLifeTable = getCompleteLifeTableWithStartAge(
-            this.testLifeTable,
-            (age) => {
-                const ageRowFromTestLifeTable = this.testLifeTable
-                    .find((testLifeTableRow: any) => {
-                        return testLifeTableRow.age === age
-                    });
-                if(!ageRowFromTestLifeTable) {
-                    throw new Error();
-                }
 
-                return 1 - Number(ageRowFromTestLifeTable.qx);
-            },
-            this.testLifeTable[0].age
-        );
-    });
+            return 1 - Number(ageRowFromTestLifeTable.qx);
+        },
+        testLifeTable[0].age,
+    );
 
-    it(`should properly calculate the life table fields`, function() {
-        this.testLifeTable.forEach((testLifeTableRow: any, index: number) => {
-            const fields = [
-                'ex'
-            ];
+    testLifeTable.forEach((testLifeTableRow: any, index: number) => {
+        const fields: [keyof CompleteLifeTableRow] = ['ex'];
 
-            /*fs.writeFileSync(
-                path.join(__dirname, '../../assets/calculated-life-table.json'),
-                JSON.stringify(this.calculatedLifeTable)
-            );*/
-            
-            fields.forEach((field) => {
-                const diff = this.calculatedLifeTable[index][field] - Number(
-                    testLifeTableRow[field]
-                );
-                const errorPercentage = diff/Number(testLifeTableRow[field]);
-                const allowedErrorPercentage = 0.01;
+        /*fs.writeFileSync(
+            path.join(__dirname, '../../assets/calculated-life-table.json'),
+            JSON.stringify(this.calculatedLifeTable)
+        );*/
 
-                expect(errorPercentage).to.be
-                    .lessThan(allowedErrorPercentage, `Row ${index} and field ${field}`)
-            });
+        fields.forEach(field => {
+            const diff =
+                calculatedLifeTable[index][field] -
+                Number(testLifeTableRow[field]);
+            const errorPercentage = diff / Number(testLifeTableRow[field]);
+            const allowedErrorPercentage = 0.01;
+
+            expect(errorPercentage).to.be.lessThan(
+                allowedErrorPercentage,
+                `Row ${index} and field ${field}`,
+            );
         });
     });
+
+    t.pass(`should properly calculate the life table fields`);
+    t.end();
 });
