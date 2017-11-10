9e3f35307f905054d14eaf63bdc4cba74037d34e
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-11-10 16:38:13 -0500

    [Refactor] Redid the library exports for browser, index and umd to account for the new builder api's

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { AlgorithmBuilder } from './engine/algorithm-builder/algorithm-builder';
+export {
+    SurvivalModelBuilder,
+} from './engine/survival-model-builder/survival-model-builder';
+export { LifeTableFunctionsBuilder } from './engine/life-table';
+export { RefPopFunctionsBuilder } from './engine/ref-pop';
