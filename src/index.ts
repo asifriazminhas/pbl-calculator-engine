488abb3bdb09f6726b2fe4f7dc72398fbc5def22
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-11-13 15:16:32 -0500

    [Feature] Export getForRiskFactorFunction from index, browser and umd module

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export {
 } from './engine/survival-model-builder/survival-model-builder';
 export { LifeTableFunctionsBuilder } from './engine/life-table';
 export { RefPopFunctionsBuilder } from './engine/ref-pop';
+export { getForRiskFactorFunction } from './engine/cause-impact';
--- src/umd.ts
+++ src/umd.ts
@@ -3,3 +3,4 @@ export {
 } from './engine/survival-model-builder/browser-survival-model-builder';
 export { LifeTableFunctionsBuilder } from './engine/life-table';
 export { RefPopFunctionsBuilder } from './engine/ref-pop';
+export { getForRiskFactorFunction } from './engine/cause-impact';
