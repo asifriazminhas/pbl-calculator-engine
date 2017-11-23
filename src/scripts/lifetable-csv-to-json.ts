eb568f8740eee176215a5958bc4a82ce1317e029
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-11-23 13:40:58 -0500

    Formatted lifetable-csv-to-json module

--- src/scripts/lifetable-csv-to-json.ts
+++ src/scripts/lifetable-csv-to-json.ts
@@ -1,50 +1,42 @@
 import * as yargs from 'yargs';
-var csvParse = require('csv-parse/lib/sync');
+// tslint:disable-next-line
+const csvParse = require('csv-parse/lib/sync');
 import * as fs from 'fs';
 import * as path from 'path';
 
 const argv = yargs
-    .usage(`Usage: npm run lifetable-csv-to-json -- <path-to-csv-file> <optional-path-to-put-json>`)
-    .demandCommand(1)
-    .argv;
+    .usage(
+        `Usage: npm run lifetable-csv-to-json -- <path-to-csv-file> <optional-path-to-put-json>`,
+    )
+    .demandCommand(1).argv;
 
-const lifeTableCsvString = fs.readFileSync(
-    path.resolve(argv._[0]),
-    'utf8'
-);
+const lifeTableCsvString = fs.readFileSync(path.resolve(argv._[0]), 'utf8');
 
 const lifeTableCsv: Array<{
     age: string;
     ax: string;
     ex: string;
 }> = csvParse(lifeTableCsvString, {
-    columns: true
+    columns: true,
 });
 
 const lifeTableJson: Array<{
     age: number;
     ax: number;
-    ex: number
-}> = lifeTableCsv
-    .map((lifeTableCsvRow) => {
-        return {
-            age: Number(lifeTableCsvRow.age),
-            ax: Number(lifeTableCsvRow.ax),
-            ex: Number(lifeTableCsvRow.ex)
-        }
-    });
+    ex: number;
+}> = lifeTableCsv.map(lifeTableCsvRow => {
+    return {
+        age: Number(lifeTableCsvRow.age),
+        ax: Number(lifeTableCsvRow.ax),
+        ex: Number(lifeTableCsvRow.ex),
+    };
+});
 
 const userGivenOutPath = argv._[1];
-const lifeTableJsonOutPath = userGivenOutPath ? (
-    path.resolve(userGivenOutPath)
-) : (
-    path.join(process.cwd(), '/life-table.json')
-);
+const lifeTableJsonOutPath = userGivenOutPath
+    ? path.resolve(userGivenOutPath)
+    : path.join(process.cwd(), '/life-table.json');
 
-fs.writeFileSync(
-    lifeTableJsonOutPath,
-    JSON.stringify(lifeTableJson)
-);
+fs.writeFileSync(lifeTableJsonOutPath, JSON.stringify(lifeTableJson));
 
 console.log(`Done. File is available at ${lifeTableJsonOutPath}`);
-
