2282f8b3a5f04377e7a70e92ee142b8d16675365
Yulric <yulrics@OHRIMAC990212.local>
2017-12-05 16:34:55 -0500

    [Feature] Evalue simple algorithm (like Sodium)

--- src/engine/simple-algorithm/simple-algorithm.ts
+++ src/engine/simple-algorithm/simple-algorithm.ts
@@ -1,7 +1,17 @@
 import { Algorithm, AlgorithmType } from '../algorithm';
 import { IGenericSimpleAlgorithm } from './generic-simple-algorithm';
-import { DerivedField } from '../derived-field';
+import { DerivedField, calculateCoefficent } from '../derived-field';
+import { Data } from '../data';
 
 export interface ISimpleAlgorithm
     extends Algorithm<AlgorithmType.SimpleAlgorithm>,
         IGenericSimpleAlgorithm<() => any, DerivedField> {}
+
+export function evaluate(algorithm: ISimpleAlgorithm, data: Data): number {
+    return calculateCoefficent(
+        algorithm.output,
+        data,
+        algorithm.userFunctions,
+        algorithm.tables,
+    ) as number;
+}
