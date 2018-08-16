4e45566a6aafaecee209dc939e7c0196d97e5fc8
Yulric Sequeira <yulrics@gmail.com>
2018-08-16 12:41:57 -0400

    [Refactor] Added TODO for Jsonify type

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,5 +1,7 @@
 import { NonFunctionKeys } from 'utility-types';
 
+/* TODO Make Jsonify type that iterates through all properties and nested
+ properties also and decides what to do depending on the property type*/
 export type Jsonify<T> = Pick<T, NonFunctionKeys<T>>;
 
 export type Constructor<T> = new (...args: any[]) => T;
