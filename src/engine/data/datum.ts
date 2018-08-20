f508e71ca71bcb8ce959e65cbc8940ad0f8b06ff
Yulric Sequeira <yulrics@gmail.com>
2018-08-20 10:11:04 -0400

    [Feature] Removed datumFactory method and added datumFactoryFromDataField to datum module

--- src/engine/data/datum.ts
+++ src/engine/data/datum.ts
@@ -1,16 +1,10 @@
 // import {} from '../cox/covariate';
 import { Coefficent } from './coefficent';
 import { Covariate } from '../data-field/covariate/covariate';
+import { DataField } from '../data-field/data-field';
 export interface IDatum {
     name: string;
     coefficent: Coefficent;
-}
-
-export function datumFactory(name: string, coefficent: Coefficent): IDatum {
-    return {
-        coefficent,
-        name,
-    };
 }
 
 export function datumFromCovariateReferencePointFactory(
@@ -19,5 +13,18 @@ export function datumFromCovariateReferencePointFactory(
     return {
         coefficent: covariate.referencePoint,
         name: covariate.name,
+    };
+}
+
+export function datumFactoryFromDataField(
+    dataField: DataField,
+    coefficient: Coefficent,
+): IDatum {
+    return {
+        name: dataField.name,
+        coefficent:
+            dataField.interval && typeof coefficient === 'number'
+                ? dataField.interval.limitNumber(coefficient)
+                : coefficient,
     };
 }
