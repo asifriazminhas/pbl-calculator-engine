55b7ffcd6292850cbb5bcdce024796ddb18c8d00
Luke Bailey <lukebaileyx@gmail.com>
2018-12-06 17:39:02 -0500

    update header report builder to build errors, warnings, ignored, and valid headers

--- src/engine/algorithm/algorithm.ts
+++ src/engine/algorithm/algorithm.ts
@@ -4,23 +4,64 @@ import { Data } from '../data';
 import { ICoxSurvivalAlgorithmJson } from '../../parsers/json/json-cox-survival-algorithm';
 import { parseUserFunctions } from '../../parsers/json/json-user-functions';
 
+export enum Header {
+    Age = 'Age',
+    Sex = 'Sex',
+    PackYears = 'Pack years'
+}
+
+export interface FileReport {
+    valid: Header[];
+    errors: Header[];
+    warnings: Header[];
+    ignored: string[];
+}
+
 export abstract class Algorithm {
+    /**
+     * @description Build a report based on detected headers from a file. Build a list of headers
+     * that are:
+     * valid: required and found
+     * error: required and missing
+     * warning: optional and missing
+     * ignored: extra, unnecessary headers
+     */
+    public static getHeaderReport (headers: Header[]): FileReport {
+        const allHeaders = Object.values(Header);
+
+        const validHeaders = [];
+        const missingHeaders = [];
+        const warningHeaders = [Header.Sex];
+        const ignoredHeaders = [];
+
+        for (const header of headers) {
+            if (allHeaders.indexOf(header) >= 0) {
+                validHeaders.push(header);
+            } else {
+                ignoredHeaders.push(header);
+            }
+        }
+
+        for (const header of allHeaders) {
+            if (validHeaders.indexOf(header) < 0) missingHeaders.push(header);
+        }
+
+        return {
+            valid: validHeaders,
+            errors: missingHeaders,
+            warnings: warningHeaders,
+            ignored: ignoredHeaders
+        };
+    }
+
     name: string;
     userFunctions: IUserFunctions;
     tables: ITables;
 
-    constructor(algorithmJson: ICoxSurvivalAlgorithmJson) {
+    constructor (algorithmJson: ICoxSurvivalAlgorithmJson) {
         this.name = algorithmJson.name;
         this.userFunctions = parseUserFunctions(algorithmJson.userFunctions);
         this.tables = algorithmJson.tables;
-    }
-
-    public getSomething (headers: string[]) {
-        return {
-            errors: ['Age'],
-            warnings: ['Sex'],
-            ignored: ['Weight']
-        };
     }
 
     abstract evaluate (data: Data): number;
