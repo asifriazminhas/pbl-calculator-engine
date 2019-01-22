55c118f4845becc76a41cd51166390ab5b9b0de2
yulricsequeira <yulrics@gmail.com>
2019-01-22 15:32:29 -0500

    [Bug] undefined is no longer parsed into a field object when converting a PMML DerivedField node to it's JSON object

--- src/parsers/pmml-to-json-parser/data_fields/derived_field/derived_field.ts
+++ src/parsers/pmml-to-json-parser/data_fields/derived_field/derived_field.ts
@@ -67,9 +67,9 @@ function getDerivedFromForAst(
         'userFunctions',
         'tables',
         'getValueFromTable',
+        'undefined',
     ];
 
-    /* TODO Fix bug where undefined is being included as a field */
     astTypes.visit(ast, {
         // Code like obj['age'] is a MemberExpression so to extract for example age we need to visit them
         visitMemberExpression(path: any) {
