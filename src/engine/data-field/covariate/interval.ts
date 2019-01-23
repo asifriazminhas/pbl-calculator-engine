bb41e02c63d051ec799d401aab1b7d1a9ecc4550
yulricsequeira <yulrics@gmail.com>
2019-01-23 15:23:38 -0500

    [Feature] Added validateData method to DataField - Added validateLowerMargin to Interval - Added validateHigherMargin to Interval

--- src/engine/data-field/covariate/interval.ts
+++ src/engine/data-field/covariate/interval.ts
@@ -23,4 +23,60 @@ export class Interval {
             return num;
         }
     }
+
+    /**
+     * Validates whether the num arg is greater than for an open margin or
+     * greater than or equal to for a closed margin
+     *
+     * @param {number} num value to validate
+     * @param {string} [variableName=''] Optional name of the variable this
+     * value belongs to. Used in the error message string returned.
+     * @returns {(string | true)} Returns an error message if validation fails.
+     * True if validation passes.
+     * @memberof Interval
+     */
+    validateLowerMargin(num: number, variableName: string = ''): string | true {
+        if (this.lowerMargin) {
+            const margin = this.lowerMargin.margin;
+
+            if (this.lowerMargin.isOpen) {
+                if (num <= margin) {
+                    return `Datum value for variable ${variableName} should be greater than ${margin}`;
+                } else if (num < margin) {
+                    return `Datum value for variable ${variableName} should be greater than or equal to ${margin}`;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates whether the num arg is less than for an open margin or
+     * less than or equal to for a closed margin
+     * @param {number} num value to validate
+     * @param {string} [variableName=''] Optional name of the variable this
+     * value belongs to. Used in the error message string returned.
+     * @returns {(string | true)} Returns an error message if validation fails.
+     * True if validation passes.
+     * @memberof Interval
+     */
+    validateHigherMargin(
+        num: number,
+        variableName: string = '',
+    ): string | true {
+        if (this.higherMargin) {
+            const margin = this.higherMargin.margin;
+
+            if (this.higherMargin.isOpen) {
+                if (num >= margin) {
+                    return `Datum value for variable ${variableName} should be less than ${margin}`;
+                } else if (num > margin) {
+                    return `Datum value for variable ${variableName} should be less than or equal to ${margin}`;
+                }
+            }
+        }
+
+        return true;
+    }
 }
