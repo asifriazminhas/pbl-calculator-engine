e17b2db8b5f51b85be98ae18d73c74479ce1ca66
yulricsequeira <yulrics@gmail.com>
2019-05-06 11:34:48 -0400

    [Feature] Added interface for JSON simple algorithm

---
+++ src/parsers/json/json-simple-algorithm.ts
@@ -0,0 +1,13 @@
+import { IDerivedFieldJson } from './json-derived-field';
+import { JsonSerializable } from '../../util/types';
+import { SimpleAlgorithm } from '../../engine/algorithm/simple-algorithm/simple-algorithm';
+import { Omit } from 'utility-types';
+import { IAlgorithmJson } from './json-algorithm';
+import { AlgorithmType } from './algorithm-type';
+
+export interface IJsonSimpleAlgorithm
+    extends Omit<JsonSerializable<SimpleAlgorithm>, 'output' | 'userFunctions'>,
+        IAlgorithmJson {
+    algorithmType: AlgorithmType.SimpleAlgorithm;
+    output: IDerivedFieldJson;
+}
