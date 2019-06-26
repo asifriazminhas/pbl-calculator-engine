aa48923f5e0c1fd986e45a08d253fadd8628f2fd
yulricsequeira <yulrics@gmail.com>
2019-06-26 14:26:14 -0400

    [Feature] Added cause deleted for unabridged le module

---
+++ src/cause-deleted/cause-deleted-unabridged-le.ts
@@ -0,0 +1,51 @@
+import { UnAbridgedLifeExpectancy } from '../engine/unabridged-life-expectancy';
+import { CauseDeletedModel } from './cause-deleted-risk';
+import { IExternalPredictor } from './external-predictor';
+import { CovariateGroup } from '../engine/data-field/covariate/covariate-group';
+import { Data } from '../engine/data';
+import { CauseDeletedRef } from './cause-deleted-ref';
+import { addCauseDeleted as addCauseDeletedToModel } from './cause-deleted-risk';
+
+export interface ICauseDeletedUnAbridgedLE extends UnAbridgedLifeExpectancy {
+    model: CauseDeletedModel;
+    calculateCDForIndividual: typeof calculateCDForIndividual;
+}
+
+export function addCauseDeleted(
+    abridgedLE: UnAbridgedLifeExpectancy,
+    riskFactorRef: CauseDeletedRef,
+): ICauseDeletedUnAbridgedLE {
+    return Object.setPrototypeOf(
+        Object.assign({}, abridgedLE, {
+            model: addCauseDeletedToModel(abridgedLE.model, riskFactorRef),
+        }),
+        UnAbridgedLifeExpectancy.prototype,
+    );
+}
+
+function calculateCDForIndividual(
+    this: ICauseDeletedUnAbridgedLE,
+    externalPredictors: IExternalPredictor[],
+    riskFactor: CovariateGroup,
+    individual: Data,
+): number {
+    const oldQx = this['getQx'];
+    this['getQx'] = getQx.bind(this, externalPredictors, riskFactor);
+
+    const individualCD = this.calculateForIndividual(individual);
+
+    this['getQx'] = oldQx;
+
+    return individualCD;
+}
+
+function getQx(
+    this: ICauseDeletedUnAbridgedLE,
+    externalPredictors: IExternalPredictor[],
+    riskFactor: CovariateGroup,
+    individual: Data,
+): number {
+    return this.model
+        .getAlgorithmForData(individual)
+        .getCauseDeletedRisk(externalPredictors, riskFactor, individual);
+}
